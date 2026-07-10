44bcfc5c984875abac951239de5b9264e4bf5da2
Whiteyo <bomamhot123@gmail.com>
2026-07-10 17:18:01 +0700

    update message from feature branch

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   console.log(`[${new Date().toISOString()}] Request received: ${req.url}`);
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello from demo-app! Time: ${new Date().toLocaleString()}\n`);
+  res.end(`Hello from demo-app! [FEATURE VERSION] Time: ${new Date().toLocaleString()}\n`);
 });
 
 server.listen(PORT, () => {
